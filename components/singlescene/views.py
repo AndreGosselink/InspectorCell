cccf8da9d3780f09d7b2f084a2e6fbde0edff890
jenkins <tanya.kux@gmail.com>
2019-04-25 14:52:07 +0200

    Changed Pen color

--- components/singlescene/views.py
+++ components/singlescene/views.py
@@ -1,28 +1,12 @@
 import random
 import pyqtgraph as pg
+from PyQt5.QtGui import QPen, QColor
 
 from pyqtgraph.Qt import QtGui as qg, QtCore as qc
 
 import pyqtgraph as pg
 import numpy as np
 
-app = qg.QApplication([])
-layout = pg.GraphicsLayout(border=(100,100,100))
-
-view = pg.GraphicsView()
-view.setCentralItem(layout)
-view.show()
-
-vbox0 = layout.addViewBox(lockAspect=True)
-vbox1 = layout.addViewBox(lockAspect=True)
-
-# import IPython as ip
-# ip.embed()
-
-# vbox0.addItem(poly)
-
-# scn = qg.QGraphicsScene()
-# scn.addText('Ahoi, Landratte')
 
 class Poly(pg.GraphicsObject):
 
@@ -37,11 +21,13 @@ class Poly(pg.GraphicsObject):
         pos = qc.QPointF(0, 0)
         self.setPos(pos)
 
-        pen = (200, 200, 100)
+        pen = QPen()
+        pen.setColor(QColor(255, 99, 71, 255))
+        pen.setWidth(1)
         self.setPen(pen)
 
-        pts_num = 500
-        x0, y0 = 0, 0
+        pts_num = 50 # to make contour more clear
+        x0, y0 = 100, 100 # with 0,0 contour can be drawn out of view (minus coordinates values)
         pts = []
         for i in range(pts_num):
             py = random.random()
@@ -55,14 +41,14 @@ class Poly(pg.GraphicsObject):
             else:
                 y0 -= dx
             pts.append((x0, y0))
-        
+
         self.poly = qg.QPolygonF()
         for p in pts:
             self.poly.append(qc.QPointF(*p))
 
     def _invalidateCache(self):
         self._boundingRect = None
-        
+
     def setPen(self, *args, **kwargs):
         """Set the pen for drawing the line. Allowable arguments are any that are valid
         for :func:`mkPen <pyqtgraph.mkPen>`."""
@@ -104,7 +90,7 @@ class Poly(pg.GraphicsObject):
         # p.drawLine(qc.QPointF(*ver0), qc.QPoint(*ver1))
         # p.drawLine(qc.QPointF(*hor0), qc.QPoint(*hor1))
         p.drawPolygon(self.poly)
-        
+
     def viewTransformChanged(self):
         """
         Called whenever the transformation matrix of the view has changed.
@@ -112,13 +98,22 @@ class Poly(pg.GraphicsObject):
         """
         self._invalidateCache()
 
+app = qg.QApplication([])
+layout = pg.GraphicsLayout(border=(100,100,100))
+
+view = pg.GraphicsView()
+view.setCentralItem(layout)
+view.show()
+
+vbox0 = layout.addViewBox(lockAspect=True)
+vbox1 = layout.addViewBox(lockAspect=True)
+
 vbox0.scene().addItem(Poly())
-import IPython as ip
-ip.embed()
 
 
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
     import sys
+
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         qg.QApplication.instance().exec_()
