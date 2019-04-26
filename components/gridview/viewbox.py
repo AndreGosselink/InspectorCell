c4845dd117383d50e6e5d84ec219952352313622
andreg <andreg@miltenyibiotec.de>
2019-04-26 09:29:34 +0200

    stripped down and improved dataviewer WIP

--- components/gridview/viewbox.py
+++ components/gridview/viewbox.py
@@ -15,9 +15,6 @@ from enhancer import Enhancer
 from crosshair import CrossHair
 
 
-class MergeROI(pg.RectROI):
-    pass
-
 class GridViewBoxLabel(pg.TextItem):
 
     def __init__(self, *args, **kwargs):
@@ -39,100 +36,9 @@ class GridViewBoxLabel(pg.TextItem):
         self.setHtml(html_text)
 
 
-#TODO make one viewboxmenu for all viewboxes?
-class GridViewBoxMenu(qg.QMenu):
-
-    def __init__(self, view=None):
-        super().__init__()
-
-        self.view = view
-        self.background_images = {}
-
-        bg_menu = self.addMenu('Select &Background...')
-        fg_menu = self.addMenu('Select &Foreground...')
-        self.addSeparator()
-        tag_menu = self.addMenu('Set &Tag...')
-        self.addSeparator()
-        self.enhance = self.addAction('&Enhance BG...')
-
-        # alpha channel control
-        alpha_menu = self.addMenu('&Opacity')
-        self.alpha_slider = qw.QSlider()
-        self.alpha_slider.setMinimum(0)
-        self.alpha_slider.setMaximum(100)
-        self.alpha_slider.setSingleStep(1)
-        self.alpha_slider.setPageStep(10)
-        alpha_action = qg.QWidgetAction(self)
-        alpha_action.setDefaultWidget(self.alpha_slider)
-        alpha_menu.addAction(alpha_action)
-        self.alpha_slider.valueChanged.connect(self._new_alpha)
-
-        self.selectors = {'bg': bg_menu,
-                          'fg': fg_menu,
-                          'tags': tag_menu,
-                         }
-
-        self._tag_selection = set([])
-
-    def update_selection(self, names, layer_name):
-        name_selection = set(names)
-        try:
-            name_selection.remove('None')
-        except KeyError:
-            pass # non None entry found
-        
-        name_selection = list(name_selection)
-        name_selection.sort()
-        name_selection = ['None'] + name_selection
-
-        if layer_name in ('fg', 'bg'):
-            self._set_image_selector(name_selection, layer_name)
-        elif layer_name == 'tags':
-            self._tag_selection = name_selection
-            # self._set_tag_selector(name_selection, layer_name)
-
-    def _set_image_selector(self, names, layer_name):
-        selector = self.selectors[layer_name]
-        selector.clear()
-        for a_name in names:
-            new_action = selector.addAction(
-                a_name, self.trig_dat_loading)
-            # used in the trigger to ask for correct image to load
-            new_action.callback_info = a_name, layer_name
-
-    def update(self):
-        self._set_tag_selector()
-
-    def _set_tag_selector(self):
-        selector = self.selectors['tags']
-        selector.clear()
-        if self.view.over_object <= 0:
-            return
-        for a_name in self._tag_selection:
-            new_action = selector.addAction(
-                a_name, self.trig_tag_setting)
-            # used in the trigger to ask for correct image to load
-            new_action.callback_info = a_name
-
-    def _new_alpha(self):
-        new_alpha = self.alpha_slider.value()
-        self.view.set_alpha(new_alpha / 100.0)
-
-    @qc.pyqtSlot()
-    def trig_dat_loading(self):
-        name, layer = self.sender().callback_info
-        self.view.load_image(name, layer)
-
-    @qc.pyqtSlot()
-    def trig_tag_setting(self):
-        tag = self.sender().callback_info
-        self.view.update_tag(tag)
-
-
 class GridViewBox(pg.ViewBox):
 
-    def __init__(self, default_label='N/A', grid=None, drawable=False,
-                 parent=None, enhance_callback=None):
+    def __init__(self, parent=None):
         """ Class implementing the drawing handling
         """
         super().__init__(invertY=True, parent=parent, lockAspect=True)
@@ -145,46 +51,14 @@ class GridViewBox(pg.ViewBox):
         self.draw_btn = qc.Qt.LeftButton
         self.context_btn = qc.Qt.RightButton
 
-        # containing ViewGrid
-        self.grid = grid
-        self.roi = None
-
-        # callbacks for the layers
-        #TODO really store the repos on one point in the app
-        # and let the layer data point to it only
-        self.layer_data = {'bg': {'img': None, 'rep': {}},
-                           'fg': {'img': None, 'rep': {}},
-                           }
-
-        self.last_loaded = {'bg': 'None',
-                            'fg': 'None',}
+        #TODO just a general description, what it shows like names
+        #TODO as objects are real objects now, they will handel events themselve
+        #TODO as objects are real objects now, they will handel events themselve
 
         # image label
         self.imagelabel = GridViewBoxLabel()
         self.addItem(self.imagelabel)
         self.imagelabel.setZValue(10)
-
-        self.drawable = drawable
-
-        self.over_object = -1
-        self.clicked_object = -1
-
-        self._enhance_callback = enhance_callback
-
-        # if drawable:
-        #     self.overlay_image = DrawOverlayImage()
-        # else:
-        #     self.overlay_image = OverlayImage()
-        # self.addItem(self.overlay_image)
-
-        # context menu
-        self.setMenuEnabled()
-        self.menu = GridViewBoxMenu(view=self)
-        self.menu.enhance.triggered.connect(self.req_enhancment)
-
-        # preprocessing pipeline
-        self.enhancer = Enhancer()
-        self.enhancing = False
 
         # crosshair
         self.crosshair = CrossHair()
@@ -192,151 +66,16 @@ class GridViewBox(pg.ViewBox):
         self.addItem(self.crosshair)
         self.crosshair.setZValue(9)
 
-    def set_alpha(self, value):
-        image = self.layer_data['fg']['img']
-        if not image is None:
-            image.setOpacity(value)
-        else:
-            err = ErrorEvent(msg='Could not set alpha: no foreground loaded!')
-            qc.QCoreApplication.postEvent(self.grid, err)
-
-    def fit_to_images(self):
-        image_items = [layer['img'] for layer in self.layer_data.values()]
-        self.autoRange(padding=0, items=image_items)
-
-    def update_repos(self, new_repos):
-        """distrobuting the good news
-        """
-        for layer_name in ('fg', 'bg'):
-            new_repo = new_repos[layer_name]
-            layer = self.layer_data[layer_name]
-            layer['rep'] = new_repo
-            new_selection = layer['rep'].keys()
-            self.menu.update_selection(new_selection, layer_name)
-
-        tag_callback = new_repos['tags']
-        self.update_tag_selection(*tag_callback())
-
-    def update_tag_selection(self, obj_tags, custom_tags):
-        tag_selection = list(obj_tags.union(custom_tags))
-        self.menu.update_selection(tag_selection, 'tags')
-
-    def update_tag(self, tag):
-        mod = {'tag': tag,
-               'objid': self.clicked_object,}
-        mod_res = ModifyResReq(res_type='obj.tag', modification=mod)
-        qc.QCoreApplication.postEvent(self.grid, mod_res)
-
-    def set_image(self, img_data, layer):
-        """ Sets the background of the view
-        """
-        img_classes = {'bg': BackgroundImage, 'fg': ForegroundImage,}
-        layer_z = {'bg': 0, 'fg': 1,}
-
-        img = self.layer_data[layer]['img']
-        if img is None:
-            ImgClass = img_classes[layer]
-            img = self.layer_data[layer]['img'] = ImgClass(parent=self)
-            self.addItem(img)
-
-        img.setImage(img_data)
-        img.setZValue(layer_z[layer])
-
-        if layer == 'bg' and self.enhancing:
-            img.setLookupTable(self.enhancer.get_scaling_lut(),
-                               update=True)
-        if layer == 'fg':
-            self.set_alpha(0.3)
-            self.menu.alpha_slider.setValue(30)
-
-        if self.grid.just_loaded:
-            self.fit_to_images()
-
-    def set_crosshair(self, coords):
-        self.crosshair.setPos(coords)
-
-    def set_cursor_info(self, obj_id=None, tags=[]):
-        if obj_id is None:
-            self.imagelabel.set_label_text(None, None, '')
-            self.over_object = -1
-        else:
-            tag_text = ', '.join(tags)
-            # pos_text = self.imagelabel.cur_text['misc']
-            pos_text = ' ObjID: {} | Tags: {}'.format(obj_id, tag_text)
-            self.imagelabel.set_label_text(None, None, pos_text)
-            self.over_object = obj_id
-
-    def get_bg_coord(self, pos):
-        bg = self.layer_data['bg']['img']
-        bg_coord = self.mapToItem(bg, pos)
-        return bg_coord
-
-    def _fetch_image(self, img_name, layer):
-        layer_repo = self.layer_data[layer]['rep']
-        callback = layer_repo.get(img_name, None)
-        try:
-            ret = callback()
-        except (TypeError, ValueError) as err:
-            msg = 'Could not fetch image {} for {}: {}'
-            self.post_error(msg.format(img_name, layer, str(err)))
-            ret = None
-        return ret
-
-    def _update_image(self, img_name, layer):
-        self.set_label(**{layer: img_name})# hacky...
-        self.last_loaded[layer] = img_name
-
-    def load_image(self, img_name, layer):
-        img = self._fetch_image(img_name, layer)
-        if not img is None:
-            self.set_image(img, layer)
-            self._update_image(img_name, layer)
-
     def set_label(self, bg=None, fg=None, bgcolor='#000000',
                   fgcolor='#10AA00'):
         """Sets the label
         """
         self.imagelabel.set_label_text(bg, fg, bgcolor=bgcolor, fgcolor=fgcolor)
 
-    def wipe_images(self):
-        """Clears everything
-        """
-        self.set_label(self.default_label)
-        for layer in ('fg', 'bg'):
-            layer_repo = self.layer_data[layer]['rep']
-            try:
-                blank_callback = layer_repo['None']
-                self.set_image(blank_callback(), layer)
-            except (KeyError, AttributeError):
-                pass # nothing to wipe then
-
-    def post_error(self, msg):
-        err = ErrorEvent(msg)
-        qc.QCoreApplication.postEvent(self.grid, err)
-
     def setRange(self, *args, **kwargs):
         super().setRange(*args, **kwargs)
         (new_x, _), (new_y, _) = self.state['viewRange']
         self.imagelabel.setPos(new_x, new_y)
-
-    def redraw(self, mapslice):
-        #TODO use signaling here
-        layer = 'fg'
-        if self.last_loaded[layer] == 'None':
-            return
-        img_name = self.last_loaded[layer]
-
-        cur_opacity = self.layer_data[layer]['img'].opacity()
-        img = self._fetch_image(img_name, layer)
-        if not img is None:
-            self.set_image(img, layer)
-        self.layer_data[layer]['img'].setOpacity(cur_opacity)
-
-    @qc.pyqtSlot()
-    def req_enhancment(self):
-        self.enhancing = True
-        if not self._enhance_callback is None:
-            self._enhance_callback(self)
 
     def mouseDragEvent(self, event):
         # paning and scaling, pass the drawing button
@@ -378,33 +117,3 @@ class GridViewBox(pg.ViewBox):
             if x is not None or y is not None:
                 self.translateBy(x=x, y=y)
             self.sigRangeChangedManually.emit(self.state['mouseEnabled'])
-
-    def mouseClickEvent(self, event):
-        if event.button() == self.context_btn:
-            self.menu.update()
-            self.raiseContextMenu(event)
-            self.clicked_object = self.over_object
-            event.ignore()
-
-    def customEvent(self, event):
-        if event == ReposUpdated:
-            self.update_repos(event)
-            event.accept()
-        else:
-            event.ignore()
-
-    def show_roi(self, show=False):
-        if self.roi is None:
-            self.roi = MergeROI(
-                [0, 0], size=[20, 20], angle=0.0, invertible=False,
-                maxBounds=None, snapSize=1.0, scaleSnap=False,
-                translateSnap=False, rotateSnap=False, parent=None,
-                pen=(0, 9), movable=True, removable=False)
-            self.roi.setZValue(9)
-            self.roi.grid = self.grid
-            self.addItem(self.roi)
-        self.roi.setVisible(show)
-
-    @property
-    def tag_selection(self):
-        return self.menu._tag_selection.copy()
--- components/gridview/viewer.py
+++ components/gridview/viewer.py
@@ -11,6 +11,23 @@ from viewbox import GridViewBox
 import pyqtgraph as pg
 
 
+class ViewState():
+    """Class holding all information of the current viewer State and methods
+    to save/load the state
+    """
+    def __init__(self, rows=2, cols=2, crosshair=False):
+        self.crosshair = crosshair
+        self.gridlayout = (rows, cols)
+
+    @property
+    def rows(self):
+        return self.gridlayout[0]
+
+    @property
+    def cols(self):
+        return self.gridlayout[1]
+
+
 class Viewer():
     """Holds and generates the views
     """
@@ -18,7 +35,7 @@ class Viewer():
     def __init__(self, parent=None):
         
         # build ui part
-        self.windows = qw.QMainWindow(parent)
+        self.window = qw.QMainWindow(parent)
         self.layout = pg.GraphicsLayoutWidget(parent)
         self.window.setCentralWidget(self.layout)
 
@@ -26,18 +43,17 @@ class Viewer():
 
         # ViewBox
         self.views = {}
-        self.view_props = {'gridlayout': (2, 2),
-                           'crosshair': False}
+        self.state = ViewState()
 
     def setup_grid(self, cols, rows):
         """ sets up the main viewgrid, depending on row and col number
         """
-        if self.view_props['gridlayout'] != (cols, rows):
+        if self.state.gridlayout != (rows, cols):
             self.layout.clear()
-            self.view_props['gridlayout'] = (cols, rows)
-            self._spawn_views(cols, rows)
+            self.state.gridlayout = (rows, cols)
+            self._spawn_views(rows=rows, cols=cols)
 
-    def _spawn_views(self, cols, rows):
+    def _spawn_views(self, rows, cols):
         """ generates row * cols viewboxes with label, background and
         foreground/overlay images
         """
@@ -71,180 +87,3 @@ class Viewer():
 
         for cur_view in self.views.values():
             cur_view.blockSignals(False)
-
-
-    def update_main_view(self):
-        self.viewgrid.setup_grid(
-            rows=self.view_props['rows'],
-            cols=self.view_props['cols'],
-            crosshair=self.view_props['crosshair'],)
-
-    def connect_ui(self):
-        """Connect end and bits
-        """
-        self.menu_bar.act_quit_app.triggered.connect(self.close)
-
-    def customEvent(self, event):
-        """ routing of my custom events
-        used to propagate stuff that happens to the surface
-        """
-        if event == LoadResReq:
-            self.req_load_res(event)
-            event.accept()
-        elif event == SaveResReq:
-            self.req_save_res(event)
-            event.accept()
-        elif event == NewViewProps:
-            self.view_props.update(event.view_props)
-            self.update_main_view()
-            event.accept()
-        elif event == ReposUpdated:
-            self.viewgrid.event(event)
-            self.menu_bar.event(event)
-        elif event == GenOverlayReq:
-            self.gen_overlay_req(event)
-        elif event == ModifyResReq:
-            self.modify_res(event)
-        elif event == ModifyView:
-            self.viewgrid.event(event)
-        elif event == ErrorEvent:
-            self.log.error(event.msg)
-            self.statusBar().showMessage(event.msg)
-            event.accept()
-        else:
-            event.ignore()
-
-    def gen_overlay_req(self, event):
-        if event.req_type == 'tagmap':
-            self.sig_req_tag_overlay.emit(list(event.args), str(event.name))
-        else:
-            self.log.error('Unknown request type: %s', event.req_type)
-        event.accept()
-
-    def modify_res(self, event):
-        if event.res_type == 'tags':
-            self.sig_req_modify_tags.emit(list(event.modification))
-        elif event.res_type == 'obj.tag':
-            obj_id = int(event.modification['objid'])
-            tag = str(event.modification['tag'])
-            self.sig_req_modify_objtag.emit(obj_id, tag)
-        elif event.res_type == 'obj.reduce':
-            self.sig_req_reduce_obj.emit(event.modification)
-        elif event.res_type == 'obj.merge':
-            self.sig_req_merge_obj.emit(event.modification)
-        elif event.res_type == 'obj.delete':
-            self.sig_req_delete_obj.emit(event.modification)
-        elif event.res_type == 'obj.create':
-            self.sig_req_create_obj.emit(event.modification)
-        elif event.res_type == 'obj.scalar':
-            self.sig_req_change_scalar.emit(event.modification)
-        else:
-            self.log.error('Unknown request type: %s', event.req_type)
-        event.accept()
-
-    def req_load_res(self, event):
-        if event.res_type == 'img.dir':
-            self.sig_req_load_imagedir.emit(event.res_path)
-        elif event.res_type == 'img.sel':
-            self.sig_req_load_imagesel.emit(event.res_pathes)
-        elif event.res_type == 'overlay.map':
-            self.sig_req_load_overlaymaps.emit(event.res_pathes)
-        elif event.res_type == 'obj.map':
-            self.sig_req_load_objmap.emit(event.res_path)
-        elif event.res_type == 'obj.zip':
-            self.sig_req_load_objzip.emit(event.res_path)
-        elif event.res_type == 'obj.tag':
-            self.sig_req_load_objtags.emit(event.res_path)
-        elif event.res_type == 'view.yaml':
-            self.load_view_state(event.res_path)
-        else:
-            self.log.debug('Cant handle {}'.format(event.res_type))
-            event.ignore()
-
-    def req_save_res(self, event):
-        if event.res_type == 'obj.zip':
-            self.sig_req_save_objzip.emit(event.res_path)
-        elif event.res_type == 'view.yaml':
-            self.save_view_state(event.res_path)
-
-    def save_view_state(self, path):
-        path = Path(path)
-        grid = self.viewgrid
-
-        state = {
-            'layout': list(grid.layout),
-            'tag_sel': [],
-            'view_setups': [],
-        }
-
-        ref_view = grid.views.get((0, 0), None)
-        state['tag_sel'] = list(ref_view.tag_selection)
-        if not ref_view is None:
-            state['range'] = ref_view.viewRange()
-        else:
-            state['range'] = None
-
-        view_setups = state['view_setups']
-        for idx, view in grid.views.items():
-            try:
-                fg_op = view.layer_data['fg']['img'].opacity()
-            except AttributeError:
-                fg_op = None
-            if view.enhancing is False:
-                enhancement = None
-            else:
-                enhancement = [list(view.enhancer.adjust_from),
-                               list(view.enhancer.adjust_to)]
-            view_state = {
-                'fg': view.last_loaded['fg'],
-                'bg': view.last_loaded['bg'],
-                'bg_enhance': enhancement,
-                'fg_alpha': fg_op,
-                'index': list(idx),}
-            view_setups.append(view_state)
-
-        with path.open('w') as yml:
-            yaml.dump(state, yml)
-
-    def load_view_state(self, path):
-        path = Path(path)
-        grid = self.viewgrid
-
-        with path.open('r') as yml:
-            state = yaml.load(yml)
-
-        cols, rows = state['layout']
-        # self.log.debug('restoring layout (%d, %d)', cols, rows)
-        grid.setup_grid(cols, rows)
-        # self.log.debug('restoring tags %s', str(state['tag_sel']))
-        self.sig_req_modify_tags.emit(state['tag_sel'])
-
-        grid.just_loaded = False
-
-        for view_state in state['view_setups']:
-            idx = tuple(view_state['index'])
-            view = grid.views[idx]
-
-            enhancement = view_state['bg_enhance']
-            if not enhancement is None:
-                adj_from, adj_to = enhancement
-                view.enhancer.adjust_from = adj_from
-                view.enhancer.adjust_to = adj_to
-                view.enhancing = True
-            else:
-                view.enhancing = False
-
-            for layer in ('bg', 'fg'):
-                img_name = view_state[layer]
-                view.load_image(img_name, layer)
-            fg_alpha = view_state['fg_alpha']
-
-            if not fg_alpha is None:
-                view.set_alpha(fg_alpha)
-
-        vrange = state['range']
-        ref_view = grid.views.get((0, 0), None)
-        new_xrange, new_yrange = vrange
-        ref_view.setRange(
-            xRange=new_xrange, yRange=new_yrange, padding=0)
-
